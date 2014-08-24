373d74fc784546d1affe52b82066b8cec8f2d60d
TonyEx <tonyangelox@Veronica.local>
2014-08-24 18:12:30 -0400

    Initial Commit

---
+++ A View Into the World/TEAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEAViewController.h
+//  A View Into the World
+//
+//  Created by Tony Angelo on 8/24/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAViewController : UIViewController
+
+@end
---
+++ A View Into the World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  A View Into the World
+//
+//  Created by Tony Angelo on 8/24/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TEAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TEAAppDelegate class]));
+	}
+}
